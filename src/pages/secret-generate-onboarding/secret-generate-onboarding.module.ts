e5c02069501d10ba05941305b6c1b2b4a2572c7f
Andreas Gassmann <andreas@andreasgassmann.ch>
2018-11-26 14:11:29 +0100

    feat(prettier): add html formatting

--- src/pages/secret-generate-onboarding/secret-generate-onboarding.module.ts
+++ src/pages/secret-generate-onboarding/secret-generate-onboarding.module.ts
@@ -1,13 +1,9 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { SecretGenerateOnboardingPage } from './secret-generate-onboarding';
+import { NgModule } from '@angular/core'
+import { IonicPageModule } from 'ionic-angular'
+import { SecretGenerateOnboardingPage } from './secret-generate-onboarding'
 
 @NgModule({
-  declarations: [
-    SecretGenerateOnboardingPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(SecretGenerateOnboardingPage),
-  ],
+  declarations: [SecretGenerateOnboardingPage],
+  imports: [IonicPageModule.forChild(SecretGenerateOnboardingPage)]
 })
 export class SecretGenerateOnboardingPageModule {}
