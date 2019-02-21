66e9040a9af1b8dc48de7988efbb9e529f1cb926
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-02-21 17:27:08 +0100

    fix(test): add missing imports

--- src/pages/transaction-signed/transaction-signed.spec.ts
+++ src/pages/transaction-signed/transaction-signed.spec.ts
@@ -1,9 +1,11 @@
 import 'jasmine'
 import { async, TestBed, ComponentFixture } from '@angular/core/testing'
 import { IonicModule, NavController, NavParams, Platform } from 'ionic-angular'
+import { QRCodeModule } from 'angularx-qrcode'
 import { TransactionSignedPage } from './transaction-signed'
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
+import { Clipboard } from '@ionic-native/clipboard'
 
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavParamsMock } from '../../../test-config/mocks-ionic'
 import { NavControllerMock } from 'ionic-mocks'
@@ -11,7 +13,6 @@ import { NavControllerMock } from 'ionic-mocks'
 import { ComponentsModule } from '../../components/components.module'
 import { SecretsProvider } from '../../providers/secrets/secrets.provider'
 import { SecureStorageService } from '../../providers/storage/secure-storage'
-import { QRCodeModule } from 'angularx-qrcode'
 import { WalletMock } from '../../../test-config/wallet-mock'
 import { StorageMock } from '../../../test-config/storage-mock'
 import { Storage } from '@ionic/storage'
@@ -20,6 +21,7 @@ import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-transla
 import { HttpLoaderFactory } from '../../app/app.module'
 import { HttpClient } from '@angular/common/http'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { ClipboardProvider } from '../../providers/clipboard/clipboard'
 
 describe('TransactionSigned Page', () => {
   const ethWallet = new WalletMock().ethWallet
@@ -51,6 +53,8 @@ describe('TransactionSigned Page', () => {
         })
       ],
       providers: [
+        Clipboard,
+        ClipboardProvider,
         SecretsProvider,
         { provide: SecureStorageService, useClass: SecureStorageServiceMock },
         { provide: Storage, useClass: StorageMock },
