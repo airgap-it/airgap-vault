8cd65cda2ab7144b3624ddff004044bdc93a7a4c
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-06-07 10:58:38 +0200

    fix(ionic4): social recovery page

--- src/app/pages/social-recovery-import/social-recovery-import.module.ts
+++ src/app/pages/social-recovery-import/social-recovery-import.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule, Routes } from '@angular/router'
 import { IonicModule } from '@ionic/angular'
 import { TranslateModule } from '@ngx-translate/core'
@@ -15,7 +15,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RouterModule.forChild(routes), TranslateModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, RouterModule.forChild(routes), TranslateModule],
   declarations: [SocialRecoveryImportPage]
 })
 export class SocialRecoveryImportPageModule {}
