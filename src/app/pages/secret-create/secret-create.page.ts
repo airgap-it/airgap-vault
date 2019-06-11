e256dc1fec4612386ab0ddfc1a26bb40f38e85b5
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-06-11 13:03:05 +0200

    feat(ionic4): remove unused code

--- src/app/pages/secret-create/secret-create.page.ts
+++ src/app/pages/secret-create/secret-create.page.ts
@@ -1,26 +1,21 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { Storage } from '@ionic/storage'
 
 import { ErrorCategory, handleErrorLocal } from '../../services/error-handler/error-handler.service'
+import { NavigationService } from '../../services/navigation/navigation.service'
 import { SecretsService } from '../../services/secrets/secrets.service'
-import { NavigationService } from 'src/app/services/navigation/navigation.service'
 
 @Component({
   selector: 'airgap-secret-create',
   templateUrl: './secret-create.page.html',
   styleUrls: ['./secret-create.page.scss']
 })
-export class SecretCreatePage implements OnInit {
+export class SecretCreatePage {
   constructor(
     private readonly navigationService: NavigationService,
     private readonly secretsService: SecretsService,
     private readonly storage: Storage
   ) {}
-
-  public ngOnInit(): void {
-    // console.log(this.router.getCurrentNavigation())
-    // console.log(this.router.getCurrentNavigation().extras.state)
-  }
 
   public canGoBack(): boolean {
     return this.secretsService.currentSecretsList.getValue().length > 0
