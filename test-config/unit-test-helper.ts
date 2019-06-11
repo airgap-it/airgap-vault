55b650d9fdb7e22f5b9345da3d599519b0dfb7da
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-06-11 10:15:16 +0200

    feat(ionic4): mocks

---
+++ test-config/unit-test-helper.ts
@@ -0,0 +1,77 @@
+import { CommonModule } from '@angular/common'
+import { HttpClientModule } from '@angular/common/http'
+import { TestModuleMetadata } from '@angular/core/testing'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AlertController, IonicModule, NavController, Platform, ToastController } from '@ionic/angular'
+import { IonicStorageModule, Storage } from '@ionic/storage'
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core'
+
+import { ComponentsModule } from '../src/app/components/components.module'
+import { PipesModule } from '../src/app/pipes/pipes.module'
+
+import {
+  AlertControllerMock,
+  AppVersionMock,
+  DeeplinkMock,
+  LoadingControllerMock,
+  ModalControllerMock,
+  NavControllerMock,
+  PlatformMock,
+  SplashScreenMock,
+  StatusBarMock,
+  ToastControllerMock
+} from './ionic-mocks'
+import { StorageMock } from './storage-mock'
+
+export class UnitHelper {
+  public readonly mockRefs = {
+    appVersion: new AppVersionMock(),
+    platform: new PlatformMock(),
+    statusBar: new StatusBarMock(),
+    splashScreen: new SplashScreenMock(),
+    deeplink: new DeeplinkMock(),
+    toastController: new ToastControllerMock(),
+    alertController: new AlertControllerMock(),
+    loadingController: new LoadingControllerMock(),
+    modalController: new ModalControllerMock()
+  }
+
+  public testBed(testBed: TestModuleMetadata, useIonicOnlyTestBed: boolean = false): TestModuleMetadata {
+    const mandatoryDeclarations: any[] = []
+    const mandatoryImports: any[] = [
+      CommonModule,
+      ReactiveFormsModule,
+      IonicModule,
+      FormsModule,
+      RouterTestingModule,
+      HttpClientModule,
+      ComponentsModule,
+      IonicStorageModule.forRoot({
+        name: '__test_airgap_storage',
+        driverOrder: ['localstorage']
+      }),
+      TranslateModule.forRoot({
+        loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+      })
+    ]
+    const mandatoryProviders: any[] = [
+      { provide: NavController, useClass: NavControllerMock },
+      { provide: Platform, useValue: this.mockRefs.platform },
+      { provide: ToastController, useValue: this.mockRefs.toastController },
+      { provide: AlertController, useValue: this.mockRefs.alertController }
+    ]
+
+    if (!useIonicOnlyTestBed) {
+      mandatoryProviders.push({ provide: Storage, useClass: StorageMock })
+      mandatoryDeclarations.push()
+      mandatoryImports.push(PipesModule)
+    }
+
+    testBed.declarations = [...(testBed.declarations || []), ...mandatoryDeclarations]
+    testBed.imports = [...(testBed.imports || []), ...mandatoryImports]
+    testBed.providers = [...(testBed.providers || []), ...mandatoryProviders]
+
+    return testBed
+  }
+}
