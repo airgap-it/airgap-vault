bca1d28a87e12aaf56861830f20bcfc8f6f79de4
Amadeo Charle <amadeo.charle108@gmail.com>
2019-06-15 22:48:03 +0200

    Merge branch 'feat/ionic-4' into 'fix/ionic-4-unit-tests'
    # Conflicts:
    #   src/app/services/secrets/secrets.service.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,58 @@
-/*
+import { UnitHelper } from './../../test-config/unit-test-helper'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { async, TestBed } from '@angular/core/testing'
+import { TestBed, ComponentFixture } from '@angular/core/testing'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx'
 import { StatusBar } from '@ionic-native/status-bar/ngx'
 import { Platform } from '@ionic/angular'
 
 import { AppComponent } from './app.component'
+import { Deeplinks } from '@ionic-native/deeplinks/ngx'
+import { StartupChecksService } from './services/startup-checks/startup-checks.service'
+import { SchemeRoutingService } from './services/scheme-routing/scheme-routing.service'
+import { TranslateService } from '@ngx-translate/core'
+import { ProtocolsService } from './services/protocols/protocols.service'
+import { SecretsService } from './services/secrets/secrets.service'
+import { NavigationService } from './services/navigation/navigation.service'
 
 describe('AppComponent', () => {
   let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy
-
-  beforeEach(async(() => {
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>
+  let unitHelper: UnitHelper
+  beforeEach(() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault'])
     splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide'])
     platformReadySpy = Promise.resolve()
     platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy })
 
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        { provide: StatusBar, useValue: statusBarSpy },
-        { provide: SplashScreen, useValue: splashScreenSpy },
-        { provide: Platform, useValue: platformSpy }
-      ]
-    }).compileComponents()
-  }))
+    unitHelper = new UnitHelper()
+    TestBed.configureTestingModule(
+      unitHelper.testBed({
+        declarations: [AppComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          { provide: StatusBar, useValue: statusBarSpy },
+          { provide: SplashScreen, useValue: splashScreenSpy },
+          { provide: Platform, useValue: platformSpy },
+          Deeplinks,
+          StartupChecksService,
+          SchemeRoutingService,
+          TranslateService,
+          ProtocolsService,
+          SecretsService,
+          NavigationService
+        ]
+      })
+    )
+      .compileComponents()
+      .catch(console.error)
+  })
+
+  beforeEach(async () => {
+    localStorage.clear()
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
+  })
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent)
@@ -33,14 +60,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it('should initialize the app', async () => {
-    TestBed.createComponent(AppComponent)
-    expect(platformSpy.ready).toHaveBeenCalled()
-    await platformReadySpy
-    expect(statusBarSpy.styleDefault).toHaveBeenCalled()
-    expect(splashScreenSpy.hide).toHaveBeenCalled()
-  })
+  // // TODO: Enable when all native parts are mocked and we can run it as "cordova"
+
+  // it('should initialize the app', async () => {
+  //   TestBed.createComponent(AppComponent)
+  //   expect(platformSpy.ready).toHaveBeenCalled()
+  //   await platformReadySpy
+  //   expect(statusBarSpy.styleDefault).toHaveBeenCalled()
+  //   expect(splashScreenSpy.hide).toHaveBeenCalled()
+  // })
 
   // TODO: add more tests!
 })
-*/
--- src/app/components/signed-transaction/signed-transaction.component.spec.ts
+++ src/app/components/signed-transaction/signed-transaction.component.spec.ts
@@ -1,28 +1,80 @@
-/*
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { EncodedType, SyncProtocolUtils } from 'airgap-coin-lib'
+
+import { UnitHelper } from '../../../../test-config/unit-test-helper'
 
 import { SignedTransactionComponent } from './signed-transaction.component'
 
 describe('SignedTransactionComponent', () => {
-  let component: SignedTransactionComponent
-  let fixture: ComponentFixture<SignedTransactionComponent>
+  let signedTransactionFixture: ComponentFixture<SignedTransactionComponent>
+  let signedTransaction: SignedTransactionComponent
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [SignedTransactionComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents()
+  let unitHelper: UnitHelper
+  beforeEach(() => {
+    unitHelper = new UnitHelper()
+    TestBed.configureTestingModule(
+      unitHelper.testBed({
+        declarations: []
+      })
+    )
+      .compileComponents()
+      .catch(console.error)
+  })
+
+  beforeEach(async () => {
+    signedTransactionFixture = TestBed.createComponent(SignedTransactionComponent)
+    signedTransaction = signedTransactionFixture.componentInstance
+  })
+
+  it('should be created', () => {
+    expect(signedTransaction instanceof SignedTransactionComponent).toBe(true)
+  })
+
+  it('should load the from-to component if a valid tx is given', async(async () => {
+    const syncProtocol = new SyncProtocolUtils()
+    const serializedTx = await syncProtocol.serialize({
+      version: 1,
+      protocol: 'eth',
+      type: EncodedType.SIGNED_TRANSACTION,
+      payload: {
+        accountIdentifier: 'test',
+        transaction:
+          'f86c808504a817c800825208944a1e1d37462a422873bfccb1e705b05cc4bd922e880de0b6b3a76400008026a00678aaa8f8fd478952bf46044589f5489e809c5ae5717dfe6893490b1f98b441a06a82b82dad7c3232968ec3aa2bba32879b3ecdb877934915d7e65e095fe53d5d'
+      }
+    })
+
+    expect(signedTransaction.airGapTx).toBe(undefined)
+    expect(signedTransaction.fallbackActivated).toBe(false)
+
+    const signedTx = await syncProtocol.deserialize(serializedTx)
+    signedTransaction.signedTx = signedTx
+    await signedTransaction.ngOnChanges()
+
+    expect(signedTransaction.airGapTx).toBeDefined()
+    expect(signedTransaction.fallbackActivated).toBe(false)
   }))
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignedTransactionComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
-  })
+  it('should load fallback if something about the TX is wrong', async(async () => {
+    const syncProtocol = new SyncProtocolUtils()
+    const serializedTx = await syncProtocol.serialize({
+      version: 1,
+      protocol: 'eth',
+      type: EncodedType.SIGNED_TRANSACTION,
+      payload: {
+        accountIdentifier: 'test',
+        transaction:
+          'asdasdasdasdsad944a1e1d37462a422873bfccb1e705b05cc4bd922e880de0b6b3a76400008026a00678aaa8f8fd478952bf46044589f5489e809c5ae5717dfe6893490b1f98b441a06a82b82dad7c3232968ec3aa2bba32879b3ecdb877934915d7e65e095fe53d5d'
+      }
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+    expect(signedTransaction.airGapTx).toBe(undefined)
+    expect(signedTransaction.fallbackActivated).toBe(false)
+
+    const signedTx = await syncProtocol.deserialize(serializedTx)
+    signedTransaction.signedTx = signedTx
+    await signedTransaction.ngOnChanges()
+
+    expect(signedTransaction.airGapTx).toBeUndefined()
+    expect(signedTransaction.fallbackActivated).toBe(true)
+  }))
 })
-*/
