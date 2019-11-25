72fafce8fcb343bd928e49f1a4204bddb4e002d0
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-11-25 10:56:19 +0100

    feat(transaction): add transaction details

--- src/app/components/from-to/from-to.component.ts
+++ src/app/components/from-to/from-to.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core'
 
 import { Transaction } from '../../models/transaction.model'
-import { IAirGapTransaction } from 'airgap-coin-lib';
+import { IAirGapTransaction } from 'airgap-coin-lib'
 
 @Component({
   selector: 'airgap-from-to',
@@ -11,6 +11,8 @@ import { IAirGapTransaction } from 'airgap-coin-lib';
 export class FromToComponent {
   @Input()
   public transaction: Transaction | IAirGapTransaction
+
+  public displayRawData: boolean = false
 
   constructor() {}
 }
