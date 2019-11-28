37b472a595877df37b29630f8997394fcd88d530
acharl <amadeo@fermat.org>
2019-11-28 13:34:36 +0100

    Revert "feat(app): adds tzbtc to supported protocols"
    This reverts commit 7c4db8e5712856fc4ef1b1d4e2b3ce27118bd91e.

--- src/app/services/protocols/protocols.service.ts
+++ src/app/services/protocols/protocols.service.ts
@@ -10,7 +10,6 @@ import {
   UnsignedTransaction
 } from 'airgap-coin-lib'
 import { AeternityERC20Token } from 'airgap-coin-lib/dist/protocols/ethereum/erc20/AeToken'
-import { TezosBTC } from 'airgap-coin-lib/dist/protocols/tezos/fa/TezosBTC'
 
 import { tokens } from './tokens'
 export interface Token {
@@ -54,7 +53,6 @@ export class ProtocolsService {
   public addProtocols() {
     addSupportedProtocol(AeternityERC20Token)
     addSubProtocol('xtz', new TezosKtProtocol())
-    addSubProtocol('xtz', new TezosBTC())
 
     this.subProtocols.forEach(supportedSubAccount => {
       supportedSubAccount.subProtocols.forEach(subProtocol => {
