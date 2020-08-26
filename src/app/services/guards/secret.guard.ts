0587c1a6f05a4520285d3a5908a990ed771e079c
Tim Schluchter <t.schluchter@papers.ch>
2020-08-26 17:45:46 +0200

    fix(secret.guard): build issue

--- src/app/services/guards/secret.guard.ts
+++ src/app/services/guards/secret.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { CanActivate, ActivatedRouteSnapshot } from '@angular/router'
 import { Injectable } from '@angular/core'
 import { NavigationService } from '../navigation/navigation.service'
 import { SecretsService } from '../secrets/secrets.service'
