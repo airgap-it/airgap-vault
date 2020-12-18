050d30df035f1c2934140adb4534a8a60bb2fdae
Amadeo Charle <amadeo.charle108@gmail.com>
2020-12-18 15:13:56 +0100

    feat/latest-message-signing

---
+++ src/app/components/message-sign-request/message-sign-request.component.spec.ts
@@ -0,0 +1,24 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { IonicModule } from '@ionic/angular';
+
+// import { MessageSigningComponent } from './message-signing.component';
+
+// describe('MessageSigningComponent', () => {
+//   let component: MessageSigningComponent;
+//   let fixture: ComponentFixture<MessageSigningComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ MessageSigningComponent ],
+//       imports: [IonicModule.forRoot()]
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(MessageSigningComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   }));
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ src/app/components/message-sign-response/message-sign-response.component.spec.ts
@@ -0,0 +1,24 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+// import { IonicModule } from '@ionic/angular'
+
+// import { MessageSignResponseComponent } from './message-sign-response.component'
+
+// describe('MessageSignResponseComponent', () => {
+//   let component: MessageSignResponseComponent
+//   let fixture: ComponentFixture<MessageSignResponseComponent>
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [MessageSignResponseComponent],
+//       imports: [IonicModule.forRoot()]
+//     }).compileComponents()
+
+//     fixture = TestBed.createComponent(MessageSignResponseComponent)
+//     component = fixture.componentInstance
+//     fixture.detectChanges()
+//   }))
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy()
+//   })
+// })
