992aa9321c56005faa89cd372d92d8b42dc805d3
Amadeo Charle <amadeo.charle108@gmail.com>
2021-01-13 11:10:58 +0100

    Add QrSettings page

---
+++ src/app/pages/qr-settings/qr-settings.page.spec.ts
@@ -0,0 +1,24 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+// import { IonicModule } from '@ionic/angular'
+
+// import { QrSettingsPage } from './qr-settings.page'
+
+// describe('QrSettingsPage', () => {
+//   let component: QrSettingsPage
+//   let fixture: ComponentFixture<QrSettingsPage>
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [QrSettingsPage],
+//       imports: [IonicModule.forRoot()]
+//     }).compileComponents()
+
+//     fixture = TestBed.createComponent(QrSettingsPage)
+//     component = fixture.componentInstance
+//     fixture.detectChanges()
+//   }))
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy()
+//   })
+// })
