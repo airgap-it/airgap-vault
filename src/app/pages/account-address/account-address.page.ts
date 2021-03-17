c179a29a9285ab34fa244b0857dc4bda7de13458
Julia Samól <j.samol@papers.ch>
2021-03-17 11:49:44 +0100

    feat(fingerprint): migrate entries without fingerprint

--- src/app/pages/account-address/account-address.page.ts
+++ src/app/pages/account-address/account-address.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core'
-import { PopoverController } from '@ionic/angular'
+import { ClipboardService, UiEventService } from '@airgap/angular-core'
 import { AirGapWallet } from '@airgap/coinlib-core'
+import { Component } from '@angular/core'
+import { PopoverController } from '@ionic/angular'
 
-import { ClipboardService } from '@airgap/angular-core'
 import { ErrorCategory, handleErrorLocal } from '../../services/error-handler/error-handler.service'
 import { InteractionOperationType, InteractionService } from '../../services/interaction/interaction.service'
+import { MigrationService } from '../../services/migration/migration.service'
 import { NavigationService } from '../../services/navigation/navigation.service'
 import { SecretsService } from '../../services/secrets/secrets.service'
 import { ShareUrlService } from '../../services/share-url/share-url.service'
@@ -16,9 +17,11 @@ import { AccountEditPopoverComponent } from './account-edit-popover/account-edit
   templateUrl: './account-address.page.html',
   styleUrls: ['./account-address.page.scss']
 })
-export class AccountAddressPage implements OnInit {
+export class AccountAddressPage {
   public wallet: AirGapWallet
-  private walletShareUrl: string
+
+  private walletShareUrl?: string
+  private walletShareUrlPromise?: Promise<void>
 
   constructor(
     private readonly popoverCtrl: PopoverController,
@@ -26,13 +29,11 @@ export class AccountAddressPage implements OnInit {
     private readonly secretsService: SecretsService,
     private readonly shareUrlService: ShareUrlService,
     private readonly interactionService: InteractionService,
-    private readonly navigationService: NavigationService
+    private readonly navigationService: NavigationService,
+    private readonly uiEventService: UiEventService,
+    private readonly migrationService: MigrationService
   ) {
     this.wallet = this.navigationService.getState().wallet
-  }
-
-  public async ngOnInit(): Promise<void> {
-    this.walletShareUrl = await this.shareUrlService.generateShareURL(this.wallet)
   }
 
   public done(): void {
@@ -40,6 +41,8 @@ export class AccountAddressPage implements OnInit {
   }
 
   public async share(): Promise<void> {
+    await this.waitWalletShareUrl()
+
     this.interactionService.startInteraction(
       {
         operationType: InteractionOperationType.WALLET_SYNC,
@@ -50,6 +53,8 @@ export class AccountAddressPage implements OnInit {
   }
 
   public async presentEditPopover(event: Event): Promise<void> {
+    await this.waitWalletShareUrl()
+
     const popover: HTMLIonPopoverElement = await this.popoverCtrl.create({
       component: AccountEditPopoverComponent,
       componentProps: {
@@ -68,5 +73,74 @@ export class AccountAddressPage implements OnInit {
 
   public async copyAddressToClipboard(): Promise<void> {
     await this.clipboardService.copyAndShowToast(this.wallet.receivingPublicAddress)
+  }
+
+  private async waitWalletShareUrl(): Promise<void> {
+    if (this.walletShareUrl !== undefined) {
+      return
+    }
+
+    await this.assertWalletValid()
+
+    if (this.walletShareUrlPromise === undefined) {
+      this.walletShareUrlPromise = new Promise<string>(async (resolve) => {
+        const shareUrl: string = await this.shareUrlService.generateShareURL(this.wallet)
+        resolve(shareUrl)
+      }).then((shareUrl: string) => {
+        this.walletShareUrl = shareUrl
+        this.walletShareUrlPromise = undefined
+      })
+    }
+
+    return this.walletShareUrlPromise
+  }
+
+  private async assertWalletValid(): Promise<void> {
+    const migrateWallet = (bip39Passphrase: string = '', tries: number = 0): Promise<void> => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          await this.migrationService.migrateWallet(this.wallet, bip39Passphrase)
+          resolve()
+        } catch (error) {
+          if (error.message?.toLowerCase().startsWith('invalid bip-39 passphrase') && tries < 1) {
+            await this.showBip39PassphraseAlert((passphrase: string) => {
+              migrateWallet(passphrase, tries + 1)
+                .then(resolve)
+                .catch(reject)
+            })
+          } else {
+            reject(error)
+          }
+        }
+      })
+    }
+
+    await Promise.all([migrateWallet()])
+  }
+
+  private async showBip39PassphraseAlert(onSuccess?: (passphrase: string) => void) {
+    await this.uiEventService.showTranslatedAlert({
+      header: 'wallet-address.alert.bip39-passphrase.header',
+      message: 'wallet-address.alert.bip39-passphrase.message',
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'bip39Passphrase',
+          type: 'password',
+          placeholder: 'wallet-address.alert.bip39-passphrase.input-placeholder_label'
+        }
+      ],
+      buttons: [
+        {
+          text: 'wallet-address.alert.bip39-passphrase.button_label',
+          handler: async (result: { bip39Passphrase: string }): Promise<void> => {
+            const passphrase: string = result.bip39Passphrase ?? ''
+            if (onSuccess !== undefined) {
+              onSuccess(passphrase)
+            }
+          }
+        }
+      ]
+    })
   }
 }
---
+++ src/app/services/migration/migration.service.ts
@@ -0,0 +1,55 @@
+import { AirGapWallet } from '@airgap/coinlib-core'
+import { Injectable } from '@angular/core'
+import * as bip32 from 'bip32'
+import * as bip39 from 'bip39'
+
+import { Secret } from '../../models/secret'
+import { SecretsService } from '../secrets/secrets.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MigrationService {
+  constructor(private readonly secretsService: SecretsService) {}
+
+  public async migrateSecret(secret: Secret): Promise<void> {
+    if (secret.fingerprint !== undefined) {
+      const entropy: string = await this.secretsService.retrieveEntropyForSecret(secret)
+      const mnemonic: string = bip39.entropyToMnemonic(entropy)
+      const seed: Buffer = await bip39.mnemonicToSeed(mnemonic)
+      const bip: bip32.BIP32Interface = bip32.fromSeed(seed)
+      const fingerprint: string = bip.fingerprint.toString('hex')
+
+      secret.fingerprint = fingerprint
+    }
+
+    await this.secretsService.addOrUpdateSecret(secret, { setActive: false })
+  }
+
+  public async migrateWallet(wallet: AirGapWallet, bip39Passphrase: string = ''): Promise<void> {
+    if (wallet.masterFingerprint.length > 0) {
+      return
+    }
+
+    const secret: Secret | undefined = this.secretsService.findByPublicKey(wallet.publicKey)
+    if (secret === undefined) {
+      return
+    }
+
+    const entropy: string = await this.secretsService.retrieveEntropyForSecret(secret)
+    const mnemonic: string = bip39.entropyToMnemonic(entropy)
+    const publicKey: string = await wallet.protocol.getPublicKeyFromMnemonic(mnemonic, wallet.derivationPath, bip39Passphrase)
+
+    if (publicKey !== wallet.publicKey) {
+      throw new Error('Invalid BIP-39 Passphrase')
+    }
+
+    const seed: Buffer = await bip39.mnemonicToSeed(mnemonic, bip39Passphrase)
+    const bip: bip32.BIP32Interface = bip32.fromSeed(seed)
+    const fingerprint: string = bip.fingerprint.toString('hex')
+
+    wallet.masterFingerprint = fingerprint
+
+    await this.migrateSecret(secret)
+  }
+}
