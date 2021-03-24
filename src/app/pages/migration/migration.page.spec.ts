fdec218471e4cdf4a8dfb187c2ef25959a917450
Julia Samól <j.samol@papers.ch>
2021-03-24 16:44:55 +0100

    test(migration): add missing provider

--- src/app/pages/migration/migration.page.spec.ts
+++ src/app/pages/migration/migration.page.spec.ts
@@ -1,22 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-import { IonicModule } from '@ionic/angular'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { provideMockStore } from '@ngrx/store/testing'
+
+import { UnitHelper } from '../../../../test-config/unit-test-helper'
 
 import { MigrationPage } from './migration.page'
+import * as fromMigration from './migration.reducers'
 
 describe('MigrationPage', () => {
   let component: MigrationPage
   let fixture: ComponentFixture<MigrationPage>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [MigrationPage],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents()
+  let unitHelper: UnitHelper
 
-    fixture = TestBed.createComponent(MigrationPage)
-    component = fixture.componentInstance
-    fixture.detectChanges()
-  }))
+  const initialState: fromMigration.State = {
+    migration: fromMigration.initialState
+  }
+
+  beforeEach(
+    waitForAsync(() => {
+      unitHelper = new UnitHelper()
+
+      TestBed.configureTestingModule(
+        unitHelper.testBed({
+          declarations: [MigrationPage],
+          providers: [provideMockStore({ initialState })]
+        })
+      )
+        .compileComponents()
+        .catch(console.error)
+
+      fixture = TestBed.createComponent(MigrationPage)
+      component = fixture.componentInstance
+      fixture.detectChanges()
+    })
+  )
 
   it('should create', () => {
     expect(component).toBeTruthy()
