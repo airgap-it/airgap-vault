3855336c2becc733903e00dfbbbca7e56b08236e
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-04-29 14:59:40 +0000

    feat(import): add new mnemonic import

--- src/app/pages/secret-import/secret-import.page.ts
+++ src/app/pages/secret-import/secret-import.page.ts
@@ -8,6 +8,10 @@ import { ErrorCategory, handleErrorLocal } from '../../services/error-handler/er
 import { NavigationService } from '../../services/navigation/navigation.service'
 import { MnemonicValidator } from '../../validators/mnemonic.validator'
 
+import * as bip39 from 'bip39'
+
+type SingleWord = string
+
 @Component({
   selector: 'airgap-secret-import',
   templateUrl: './secret-import.page.html',
@@ -16,6 +20,42 @@ import { MnemonicValidator } from '../../validators/mnemonic.validator'
 export class SecretImportPage {
   public mnemonic: string
   public secretImportForm: FormGroup
+
+  public secretWords: SingleWord[] = []
+  public promptedWords: SingleWord[] = []
+
+  public readonly alphabets: SingleWord[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ]
+
+  selectedWordIndex: number = 0
+
+  public wordBank: SingleWord[] = bip39.wordlists.EN as any
 
   constructor(
     private readonly deviceService: DeviceService,
@@ -32,6 +72,12 @@ export class SecretImportPage {
     this.secretImportForm.valueChanges.subscribe((formGroup) => {
       this.mnemonic = formGroup.mnemonic
     })
+
+    for (var i = 0; i < 24; i++) {
+      this.secretWords.push('')
+    }
+
+    this.promptedWords = this.alphabets
   }
 
   public ionViewDidEnter(): void {
@@ -42,10 +88,68 @@ export class SecretImportPage {
     this.deviceService.disableScreenshotProtection()
   }
 
+  public selectWord(word: SingleWord): void {
+    if (this.selectedWordIndex > 23) return
+
+    this.secretWords[this.selectedWordIndex] = word
+    if (word.length === 1) {
+      const temp = this.wordBank.filter((x) => x[0] === word)
+
+      this.promptedWords = []
+
+      temp.forEach((x) => {
+        const y = x.substr(0, 2)
+        if (this.promptedWords.indexOf(y) < 0) {
+          this.promptedWords.push(y)
+        }
+      })
+
+      if (this.promptedWords.length === 1) {
+        word = this.promptedWords[0]
+        const temp = this.wordBank.filter((x) => x.substr(0, 2) === word)
+
+        this.promptedWords = temp
+      }
+    } else if (word.length === 2) {
+      const temp = this.wordBank.filter((x) => x.substr(0, 2) === word)
+
+      this.promptedWords = temp
+    } else if (word.length > 2) {
+      this.selectedWordIndex++
+      this.promptedWords = this.alphabets
+    } else {
+      this.promptedWords = this.alphabets
+    }
+  }
+
+  public undo(): void {
+    if (this.selectedWordIndex > 23) {
+      this.selectedWordIndex = 23
+    }
+
+    if (!this.secretWords[this.selectedWordIndex].length && this.selectedWordIndex > 0) {
+      this.selectedWordIndex--
+    }
+
+    if (this.secretWords[this.selectedWordIndex].length > 2) {
+      this.secretWords[this.selectedWordIndex] = this.secretWords[this.selectedWordIndex].substr(0, 2)
+    } else if (this.secretWords[this.selectedWordIndex].length === 2) {
+      this.secretWords[this.selectedWordIndex] = this.secretWords[this.selectedWordIndex][0]
+    } else if (this.secretWords[this.selectedWordIndex].length === 1) {
+      this.secretWords[this.selectedWordIndex] = ''
+    }
+
+    this.selectWord(this.secretWords[this.selectedWordIndex])
+  }
+
+  public isValid(): boolean {
+    return BIP39Signer.validateMnemonic(this.secretWords.join(' '))
+  }
+
   public goToSecretCreatePage(): void {
     const signer: BIP39Signer = new BIP39Signer()
 
-    const secret: Secret = new Secret(signer.mnemonicToEntropy(BIP39Signer.prepareMnemonic(this.mnemonic)))
+    const secret: Secret = new Secret(signer.mnemonicToEntropy(BIP39Signer.prepareMnemonic(this.secretWords.join(' '))))
 
     this.navigationService
       .routeWithState('secret-edit', { secret, isGenerating: true })
