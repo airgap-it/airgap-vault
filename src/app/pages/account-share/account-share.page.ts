bcb106c19924c5b4c4f4031f004034e9479f89be
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-05-27 01:05:04 +0300

    fix: don't break iteraction urls with a custom prefix

--- src/app/pages/account-share/account-share.page.ts
+++ src/app/pages/account-share/account-share.page.ts
@@ -15,7 +15,7 @@ export class AccountSharePage {
 
   constructor(private readonly navigationService: NavigationService) {
     this.interactionUrl = this.navigationService.getState().interactionUrl
-    this.splits = this.interactionUrl.substr('airgap-wallet://?d='.length).split(',')
+    this.splits = this.interactionUrl.replace(/^airgap-wallet:\/\/\?d=/, '').split(',')
   }
 
   public done(): void {
--- src/app/pages/transaction-signed/transaction-signed.page.ts
+++ src/app/pages/transaction-signed/transaction-signed.page.ts
@@ -33,7 +33,7 @@ export class TransactionSignedPage {
 
   constructor(public navigationService: NavigationService, private readonly translateService: TranslateService) {
     this.interactionUrl = this.navigationService.getState().interactionUrl
-    this.splits = this.interactionUrl.substr('airgap-wallet://?d='.length).split(',')
+    this.splits = this.interactionUrl.replace(/^airgap-wallet:\/\/\?d=/, '').split(',')
     this.wallets = this.navigationService.getState().wallets
     this.signedTxs = this.navigationService.getState().signedTxs
     this.translationKey = this.navigationService.getState().translationKey
