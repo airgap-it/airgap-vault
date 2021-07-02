4db0af2dea1261221a6d348330e9e4493f098461
acharl <amadeo.charle108@gmail.com>
2021-07-02 12:30:56 +0200

    feat(walletconnect): display message sign request hex data as readable string

--- src/app/components/message-sign-request/message-sign-request.component.ts
+++ src/app/components/message-sign-request/message-sign-request.component.ts
@@ -5,11 +5,24 @@ import { Component, Input } from '@angular/core'
   styleUrls: ['./message-sign-request.component.scss']
 })
 export class MessageSignRequestComponent {
-  @Input()
-  public messages: {
+  public _messages: {
     data: string
     blake2bHash?: string
   }[]
+
+  @Input()
+  set messages(messages: { data: string; blake2bHash?: string }[]) {
+    this._messages = messages.map((message) => {
+      const data = this.hexToReadable(message.data)
+      return { ...message, data }
+    })
+  }
+
+  private hexToReadable(hex: string): string {
+    var str = ''
+    for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+    return str
+  }
 
   constructor() {}
 }
