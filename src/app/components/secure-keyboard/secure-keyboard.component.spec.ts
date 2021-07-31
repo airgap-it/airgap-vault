f07ffedb08f9dfa27426f3ccc9e14e05d4cbe614
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-07-31 22:22:21 +0200

    fix(): remove invalid import

--- src/app/components/secure-keyboard/secure-keyboard.component.spec.ts
+++ src/app/components/secure-keyboard/secure-keyboard.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { IonicModule } from '@ionic/angular'
 
-import { getWordAroundPosition, SecureKeyboardComponent } from './secure-keyboard.component'
+import { SecureKeyboardComponent } from './secure-keyboard.component'
 
 describe('SecureKeyboardComponent', () => {
   let component: SecureKeyboardComponent
@@ -22,22 +22,5 @@ describe('SecureKeyboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
-  })
-
-  fit('should create', () => {
-    expect(component).toBeTruthy()
-
-    console.log(getWordAroundPosition('aaa bbb ccc', 0) === 'aaa')
-    console.log(getWordAroundPosition('aaa bbb ccc', 1) === 'aaa')
-    console.log(getWordAroundPosition('aaa bbb ccc', 2) === 'aaa')
-    console.log(getWordAroundPosition('aaa bbb ccc', 3) === 'aaa')
-    console.log(getWordAroundPosition('aaa bbb ccc', 4) === 'bbb')
-    console.log(getWordAroundPosition('aaa bbb ccc', 5) === 'bbb')
-    console.log(getWordAroundPosition('aaa bbb ccc', 6) === 'bbb')
-    console.log(getWordAroundPosition('aaa bbb ccc', 7) === 'bbb')
-    console.log(getWordAroundPosition('aaa bbb ccc', 8) === 'ccc')
-    console.log(getWordAroundPosition('aaa bbb ccc', 9) === 'ccc')
-    console.log(getWordAroundPosition('aaa bbb ccc', 10) === 'ccc')
-    console.log(getWordAroundPosition('aaa bbb ccc', 11) === 'ccc')
   })
 })
