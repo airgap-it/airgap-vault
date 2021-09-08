54acb9dc190fd688021acfa10f3f21124077be11
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-09-08 01:03:39 -0300

    fix(): remove unused test

--- src/app/pages/accounts-list/accounts-list.page.spec.ts
+++ src/app/pages/accounts-list/accounts-list.page.spec.ts
@@ -1,26 +1,26 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
-import { IonicModule } from '@ionic/angular'
+// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+// import { IonicModule } from '@ionic/angular'
 
-import { AccountsListPage } from './accounts-list.page'
+// import { AccountsListPage } from './accounts-list.page'
 
-describe('AccountsListPage', () => {
-  let component: AccountsListPage
-  let fixture: ComponentFixture<AccountsListPage>
+// describe('AccountsListPage', () => {
+//   let component: AccountsListPage
+//   let fixture: ComponentFixture<AccountsListPage>
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [AccountsListPage],
-        imports: [IonicModule.forRoot()]
-      }).compileComponents()
+//   beforeEach(
+//     waitForAsync(() => {
+//       TestBed.configureTestingModule({
+//         declarations: [AccountsListPage],
+//         imports: [IonicModule.forRoot()]
+//       }).compileComponents()
 
-      fixture = TestBed.createComponent(AccountsListPage)
-      component = fixture.componentInstance
-      fixture.detectChanges()
-    })
-  )
+//       fixture = TestBed.createComponent(AccountsListPage)
+//       component = fixture.componentInstance
+//       fixture.detectChanges()
+//     })
+//   )
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
-})
+//   it('should create', () => {
+//     expect(component).toBeTruthy()
+//   })
+// })
