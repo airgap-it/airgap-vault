fcb62fbc288e67efead4aadc188ef1ed8cd04c67
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-10-13 12:12:36 +0200

    feat(): remove disclaimer

--- src/app/pages/tab-settings/tab-settings.page.ts
+++ src/app/pages/tab-settings/tab-settings.page.ts
@@ -11,7 +11,6 @@ import { IACService } from 'src/app/services/iac/iac.service'
 import { InstallationTypePage } from '../Installation-type/installation-type.page'
 import { OnboardingAdvancedModePage } from '../onboarding-advanced-mode/onboarding-advanced-mode.page'
 import { OnboardingWelcomePage } from '../onboarding-welcome/onboarding-welcome.page'
-import { Warning, WarningModalPage } from '../warning-modal/warning-modal.page'
 
 @Component({
   selector: 'airgap-tab-settings',
@@ -61,15 +60,15 @@ export class TabSettingsPage {
     modal.present().catch(handleErrorLocal(ErrorCategory.IONIC_MODAL))
   }
 
-  public async goToDisclaimer(): Promise<void> {
-    const modal: HTMLIonModalElement = await this.modalController.create({
-      component: WarningModalPage,
-      componentProps: { errorType: Warning.INITIAL_DISCLAIMER },
-      backdropDismiss: false
-    })
+  // public async goToDisclaimer(): Promise<void> {
+  //   const modal: HTMLIonModalElement = await this.modalController.create({
+  //     component: WarningModalPage,
+  //     componentProps: { errorType: Warning.SECURE_STORAGE },
+  //     backdropDismiss: false
+  //   })
 
-    modal.present().catch(handleErrorLocal(ErrorCategory.IONIC_MODAL))
-  }
+  //   modal.present().catch(handleErrorLocal(ErrorCategory.IONIC_MODAL))
+  // }
 
   public async goToInstallationType(): Promise<void> {
     const modal: HTMLIonModalElement = await this.modalController.create({
