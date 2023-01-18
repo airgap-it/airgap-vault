482cae6ac6314c7332b24d882a662f3dd18f4ade
acharl <amadeo.charle108@gmail.com>
2023-01-18 17:20:38 +0100

    feat(UI): add popover component to display actions when importing recovery phrase

---
+++ src/app/components/keyboard-popover/keyboard-popover.component.spec.ts
@@ -0,0 +1,24 @@
+// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+// import { IonicModule } from '@ionic/angular';
+
+// import { KeyboardPopoverComponent } from './keyboard-popover.component';
+
+// describe('KeyboardPopoverComponent', () => {
+//   let component: KeyboardPopoverComponent;
+//   let fixture: ComponentFixture<KeyboardPopoverComponent>;
+
+//   beforeEach(waitForAsync(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ KeyboardPopoverComponent ],
+//       imports: [IonicModule.forRoot()]
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(KeyboardPopoverComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   }));
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
