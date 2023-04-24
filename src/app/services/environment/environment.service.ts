68ddf3e4cdc6fc952d719f5e659cabb40b6a18f3
Julia Samol <j.samol@papers.ch>
2023-04-24 10:33:09 +0200

    fix(environment): add env context listener only on mobile devices

--- src/app/services/environment/environment.service.ts
+++ src/app/services/environment/environment.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@angular/core'
+import { Platform } from '@ionic/angular'
 import { BehaviorSubject, Observable } from 'rxjs'
 import { EnvironmentPlugin } from 'src/app/capacitor-plugins/definitions'
 import { ENVIRONMENT_PLUGIN } from 'src/app/capacitor-plugins/injection-tokens'
@@ -11,10 +12,15 @@ export type EnvironmentContext = 'empty' | 'knox'
 export class EnvironmentService {
   private readonly context$: BehaviorSubject<EnvironmentContext> = new BehaviorSubject<EnvironmentContext>('empty')
 
-  constructor(@Inject(ENVIRONMENT_PLUGIN) private readonly environment: EnvironmentPlugin) {
-    this.environment.addListener('envContextChanged', ({ context }) => {
-      this.context$.next(context)
-    })
+  constructor(
+    @Inject(ENVIRONMENT_PLUGIN) private readonly environment: EnvironmentPlugin,
+    private readonly platform: Platform
+  ) {
+    if (this.platform.is('hybrid')) {
+      this.environment.addListener('envContextChanged', ({ context }) => {
+        this.context$.next(context)
+      })
+    }
   }
 
   public getContextObservable(): Observable<EnvironmentContext> {
