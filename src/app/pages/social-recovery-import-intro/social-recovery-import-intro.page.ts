b042f2e6129c6ee4df169bf0f36e66b0de56d58a
Tim <t.schluchter@papers.ch>
2023-05-30 17:43:47 +0200

    feat(): removed unused code

--- src/app/pages/social-recovery-import-intro/social-recovery-import-intro.page.ts
+++ src/app/pages/social-recovery-import-intro/social-recovery-import-intro.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { MnemonicSecret } from 'src/app/models/secret'
 import { handleErrorLocal, ErrorCategory } from 'src/app/services/error-handler/error-handler.service'
 import { NavigationService } from 'src/app/services/navigation/navigation.service'
 
@@ -9,22 +8,11 @@ import { NavigationService } from 'src/app/services/navigation/navigation.servic
   styleUrls: ['./social-recovery-import-intro.page.scss']
 })
 export class SocialRecoveryImportIntroPage implements OnInit {
-  public state: 0 | 1 | 2 = 0
-  private readonly secret: MnemonicSecret
-  constructor(private readonly navigationService: NavigationService) {
-    this.secret = this.navigationService.getState().secret
-  }
+  constructor(private readonly navigationService: NavigationService) {}
 
   ngOnInit() {}
 
   nextState() {
-    if (this.state < 2) {
-      this.state++
-      return
-    } else if (this.state >= 2) {
-      this.navigationService
-        .routeWithState('/social-recovery-import-setup', { secret: this.secret })
-        .catch(handleErrorLocal(ErrorCategory.IONIC_NAVIGATION))
-    }
+    this.navigationService.route('/social-recovery-import-setup').catch(handleErrorLocal(ErrorCategory.IONIC_NAVIGATION))
   }
 }
