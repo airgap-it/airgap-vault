d6d5cc9c0a20ac47a53d1e19b98eb3a29015dd43
Tim <t.schluchter@papers.ch>
2023-06-07 13:23:19 +0200

    feat(): added functions and step overview

--- src/app/pages/social-recovery-import-share-validate/social-recovery-import-share-validate.page.ts
+++ src/app/pages/social-recovery-import-share-validate/social-recovery-import-share-validate.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { ModalController } from '@ionic/angular'
+import { handleErrorLocal, ErrorCategory } from 'src/app/services/error-handler/error-handler.service'
+import { NavigationService } from 'src/app/services/navigation/navigation.service'
 
 @Component({
   selector: 'airgap-social-recovery-import-share-validate',
@@ -6,7 +9,38 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./social-recovery-import-share-validate.page.scss']
 })
 export class SocialRecoveryImportShareValidatePage implements OnInit {
-  constructor() {}
+  public currentShare: number = 1
+  public shares: number = 5
+
+  shareName: string = ''
+  constructor(private readonly modalController: ModalController, private navigationService: NavigationService) {}
 
   ngOnInit() {}
+
+  async help() {
+    this.modalController
+    // TODO Tim: uncomment once merged
+    // const modal: HTMLIonModalElement = await this.modalController.create({
+    //   component: SocialRecoveryImportHelpPage,
+    //   backdropDismiss: false
+    // })
+
+    // modal.present().catch(handleErrorLocal(ErrorCategory.IONIC_MODAL))
+  }
+
+  nextState() {
+    let nextStateRoute = 'soccial-recovery-import-share-name'
+
+    if (this.currentShare === this.shares) {
+      nextStateRoute = 'social-recovery-import-share-finish'
+    }
+
+    this.navigationService
+      .routeWithState(nextStateRoute, {
+        currentShare: this.currentShare + 1,
+        shares: this.shares,
+        shareName: this.shareName
+      })
+      .catch(handleErrorLocal(ErrorCategory.IONIC_NAVIGATION))
+  }
 }
