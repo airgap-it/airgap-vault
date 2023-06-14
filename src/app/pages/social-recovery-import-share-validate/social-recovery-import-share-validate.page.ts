09429a08374e882a5378830c4c1bedfc156743c8
Tim <t.schluchter@papers.ch>
2023-06-14 09:51:45 +0200

    feat(): working on new validate page

--- src/app/pages/social-recovery-import-share-validate/social-recovery-import-share-validate.page.ts
+++ src/app/pages/social-recovery-import-share-validate/social-recovery-import-share-validate.page.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { ModalController } from '@ionic/angular'
 import { handleErrorLocal, ErrorCategory } from 'src/app/services/error-handler/error-handler.service'
 import { NavigationService } from 'src/app/services/navigation/navigation.service'
+
+import * as bip39 from 'bip39'
+import { Observable, Subject } from 'rxjs'
+import { BIPSigner } from 'src/app/models/BIP39Signer'
+import { DeviceService } from 'src/app/services/device/device.service'
+
+type SingleWord = string
 
 @Component({
   selector: 'airgap-social-recovery-import-share-validate',
@@ -12,8 +19,31 @@ export class SocialRecoveryImportShareValidatePage implements OnInit {
   public currentShare: number = 1
   public shares: number = 5
 
+  public secretWords: string[] = []
+  public secretWordsValid: Observable<boolean>
+  public selectedWordIndex: number = 0
+  public selectedWord: string = ''
+
+  public maskWords: boolean = false
+
+  public wordList: SingleWord[] = bip39.wordlists.EN as any
+
+  public lastWordOptions: string[] = []
+
+  public setWordEmitter: Subject<string> = new Subject()
+
+  public keyboardEnabled: boolean = true
+
   shareName: string = ''
-  constructor(private readonly modalController: ModalController, private navigationService: NavigationService) {}
+
+  @ViewChild('secretContainer', { read: ElementRef })
+  public secretContainer: ElementRef<HTMLElement>
+
+  constructor(
+    private readonly modalController: ModalController,
+    private navigationService: NavigationService,
+    private readonly deviceService: DeviceService
+  ) {}
 
   ngOnInit() {}
 
@@ -26,6 +56,117 @@ export class SocialRecoveryImportShareValidatePage implements OnInit {
     // })
 
     // modal.present().catch(handleErrorLocal(ErrorCategory.IONIC_MODAL))
+  }
+
+  selectWord(index: number) {
+    this.selectedWordIndex = index
+    this.selectedWord = this.secretWords[this.selectedWordIndex]
+
+    this.setWordEmitter.next(this.selectedWord ?? '')
+  }
+
+  wordLastSelected(word: string | undefined) {
+    if (this.secretWords.length !== 23) {
+      return console.error('(wordLastSelected): secret word list is not 23 words long')
+    }
+    this.selectedWordIndex = 23
+    this.wordSelected(word)
+  }
+
+  wordSelected(word: string | undefined) {
+    if (typeof word === 'undefined') {
+      if (this.selectedWordIndex >= 0) {
+        this.secretWords.splice(this.selectedWordIndex, 1)
+        this.selectWord(Math.max(this.selectedWordIndex - 1, 0))
+      } else if (this.selectedWordIndex === -1) {
+        this.selectWord(this.secretWords.length - 1)
+      }
+      this.getLastWord()
+      return
+    }
+
+    if (this.selectedWordIndex === -1) {
+      this.secretWords.push(word)
+    } else {
+      this.secretWords[this.selectedWordIndex] = word
+    }
+
+    this.selectedWordIndex = -1
+    this.selectedWord = ''
+
+    this.getLastWord()
+
+    this.setWordEmitter.next(this.selectedWord ?? '')
+
+    if (this.secretContainer) {
+      this.secretContainer.nativeElement.scrollTop = this.secretContainer.nativeElement.scrollHeight
+    }
+  }
+
+  public ionViewDidEnter(): void {
+    this.deviceService.enableScreenshotProtection({ routeBack: 'secret-setup' })
+  }
+
+  public ionViewWillLeave(): void {
+    this.deviceService.disableScreenshotProtection()
+  }
+
+  public isValid(): boolean {
+    return BIPSigner.validateMnemonic(this.secretWords.join(' '))
+  }
+
+  // public goToSecretSetupPage(): void {
+  //   const signer: BIPSigner = new BIPSigner()
+  //   const secret: MnemonicSecret = new MnemonicSecret(signer.mnemonicToEntropy(BIPSigner.prepareMnemonic(this.secretWords.join(' '))))
+  //   this.navigationService.routeWithState('secret-add', { secret }).catch(handleErrorLocal(ErrorCategory.IONIC_NAVIGATION))
+  // }
+
+  // public async paste(text: string | undefined) {
+  //   if (BIPSigner.validateMnemonic(text)) {
+  //     this.secretWords = text.split(' ')
+  //     this.selectedWordIndex = -1
+  //     this.selectedWord = ''
+  //     this.setWordEmitter.next(this.selectedWord ?? '')
+  //   } else {
+  //     const alert = await this.alertController.create({
+  //       header: 'Invalid Mnemonic',
+  //       message: 'The text in your clipboard is not a valid mnemonic.',
+  //       backdropDismiss: false,
+  //       buttons: [
+  //         {
+  //           text: 'Ok'
+  //         }
+  //       ]
+  //     })
+  //     alert.present()
+  //   }
+  // }
+
+  // public async addNewWord() {
+  //   if (this.secretWords.length >= 24) {
+  //     return console.error('(addNewWord): secret word list too long')
+  //   }
+
+  //   this.secretWords.splice(this.selectedWordIndex + 1, 0, '')
+  //   this.selectedWordIndex++
+  //   this.setWordEmitter.next('')
+  // }
+
+  // public async mask(enabled: boolean) {
+  //   this.maskWords = enabled
+  // }
+
+  public getLastWord() {
+    const options = []
+    if (this.secretWords.length === 23) {
+      // The last word is 3 bits entropy and 8 bits checksum of the entropy. But because there are only 2048 words, it's fast to just try all combinations and the code is a lot easier, so we do that.
+      for (const word of bip39.wordlists.EN) {
+        if (bip39.validateMnemonic([...this.secretWords, word].join(' '))) {
+          options.push(word)
+        }
+      }
+    }
+    this.lastWordOptions = options
   }
 
   nextState() {
