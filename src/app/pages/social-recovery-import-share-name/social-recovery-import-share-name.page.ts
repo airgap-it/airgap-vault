19cf33447e6d885c515368a045c86f85d5fd131c
Tim <t.schluchter@papers.ch>
2023-06-19 14:51:17 +0200

    feat(): fixed name page state

--- src/app/pages/social-recovery-import-share-name/social-recovery-import-share-name.page.ts
+++ src/app/pages/social-recovery-import-share-name/social-recovery-import-share-name.page.ts
@@ -4,7 +4,7 @@ import { ModalController } from '@ionic/angular'
 import { handleErrorLocal, ErrorCategory } from 'src/app/services/error-handler/error-handler.service'
 import { NavigationService } from 'src/app/services/navigation/navigation.service'
 import { SocialRecoveryImportHelpPage } from '../social-recovery-import-help/social-recovery-import-help.page'
-import { SocialRecoveryImportShareService } from 'src/app/social-recovery-import-share/social-recovery-import-share.service'
+// import { SocialRecoveryImportShareService } from 'src/app/social-recovery-import-share/social-recovery-import-share.service'
 
 @Component({
   selector: 'airgap-social-recovery-import-share-name',
@@ -12,28 +12,23 @@ import { SocialRecoveryImportShareService } from 'src/app/social-recovery-import
   styleUrls: ['./social-recovery-import-share-name.page.scss']
 })
 export class SocialRecoveryImportShareNamePage implements OnInit {
-  public currentShareNumber: number = 0
+  public currentShareNumber: number
   public numberOfShares: number = 0
 
   shareName: string = ''
 
-  defaulBacktHref = this.currentShareNumber > 0 ? 'social-recovery-import-share-validate' : 'social-recovery-import-setup'
+  defaulBacktHref: string
 
   public socialRecoveryForm: FormGroup
 
-  sharesMap: Map<number, { shareName: string; share: string[] }>
+  // sharesMap: Map<number, { shareName: string; share: string[] }>
 
   constructor(
     public formBuilder: FormBuilder,
     private readonly modalController: ModalController,
-    private navigationService: NavigationService,
-    private readonly socialRecoveryImportShareService: SocialRecoveryImportShareService
+    private navigationService: NavigationService // private readonly socialRecoveryImportShareService: SocialRecoveryImportShareService
   ) {
-    const state = this.navigationService.getState()
-    this.numberOfShares = state.numberOfShares
-    this.currentShareNumber = state.currentShareNumbert
-
-    this.sharesMap = this.socialRecoveryImportShareService.getMap()
+    // this.sharesMap = this.socialRecoveryImportShareService.getMap()
 
     this.socialRecoveryForm = this.formBuilder.group({
       shareName: ''
@@ -42,9 +37,16 @@ export class SocialRecoveryImportShareNamePage implements OnInit {
     this.socialRecoveryForm.valueChanges.subscribe((val: any) => {
       if (val && val.shareName) {
         this.shareName = val.shareName
-        console.log('this sharename', this.shareName)
       }
     })
+  }
+
+  ionViewWillEnter() {
+    const state = this.navigationService?.getState()
+
+    this.numberOfShares = state.numberOfShares
+    this.currentShareNumber = state.currentShareNumber ?? 0
+    this.defaulBacktHref = this.currentShareNumber > 0 ? 'social-recovery-import-share-validate' : 'social-recovery-import-setup'
   }
 
   ngOnInit() {}
@@ -60,6 +62,7 @@ export class SocialRecoveryImportShareNamePage implements OnInit {
 
   nextState() {
     this.socialRecoveryForm.reset()
+    console.log('this currentsharenumber name page', this.currentShareNumber)
 
     this.navigationService
       .routeWithState('/social-recovery-import-share-validate', {
