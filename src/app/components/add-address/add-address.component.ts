593c041c4d1f933ecd7d6ee8c84bcf9142221792
Isacco <i.sordo@papers.ch>
2023-07-14 13:51:05 +0200

    fix: code cleanup

--- src/app/components/add-address/add-address.component.ts
+++ src/app/components/add-address/add-address.component.ts
@@ -1,59 +1,49 @@
-import { IAirGapTransaction } from '@airgap/coinlib-core/interfaces/IAirGapTransaction';
-import { Component, Input, OnInit } from '@angular/core';
-import { AddType, ContactsService } from 'src/app/services/contacts/contacts.service';
-import { ErrorCategory, handleErrorLocal } from 'src/app/services/error-handler/error-handler.service';
-import { NavigationService } from 'src/app/services/navigation/navigation.service';
+import { IAirGapTransaction } from '@airgap/coinlib-core/interfaces/IAirGapTransaction'
+import { Component, Input, OnInit } from '@angular/core'
+import { AddType, ContactsService } from 'src/app/services/contacts/contacts.service'
+import { ErrorCategory, handleErrorLocal } from 'src/app/services/error-handler/error-handler.service'
+import { NavigationService } from 'src/app/services/navigation/navigation.service'
 
 @Component({
   selector: 'airgap-add-address',
   templateUrl: './add-address.component.html',
-  styleUrls: ['./add-address.component.scss'],
+  styleUrls: ['./add-address.component.scss']
 })
-export class AddAddressComponent  implements OnInit {
+export class AddAddressComponent implements OnInit {
   public addressesNotOnContactBook: string[] = []
   @Input() private airGapTxs: IAirGapTransaction[] = []
-  // selectTransactionsDetails
-  // this.transactionsDetails$ = this.store.select(fromDeserializedDetail.selectTransactionsDetails)
 
-  constructor(private readonly navigationService: NavigationService, private readonly contactsService: ContactsService) { }
+  constructor(private readonly navigationService: NavigationService, private readonly contactsService: ContactsService) {}
 
   async ngOnInit() {
     await this.checkAdressesNames()
   }
 
-  public async checkAdressesNames() {
-    // Check for addresses in contact book
-    if (this.airGapTxs && this.airGapTxs.length > 0) {
-      const isBookenabled = await this.contactsService.isBookEnabled()
-      if (isBookenabled) {
-        this.addressesNotOnContactBook = []
-        for (let i = 0; i < this.airGapTxs.length; i++) {
-          this.airGapTxs[i].extra = { names: {} }
-          const transaction = this.airGapTxs[i]
-          const toAddresses = transaction.to
-          for (let j = 0; j < toAddresses.length; j++) {
-            const toAddress = toAddresses[j]
-            const hasContactBookAddress = await this.contactsService.isAddressInContacts(toAddress)
-            if (!hasContactBookAddress) this.addressesNotOnContactBook.push(toAddress)
-            else {
-              const name = await this.contactsService.getContactName(toAddress)
-              if (name) this.airGapTxs[i].extra.names[toAddress] = name
-            }
-          }
-          const fromAddresses = transaction.from
-          for (let j = 0; j < fromAddresses.length; j++) {
-            const fromAddress = fromAddresses[j]
-            const hasContactBookAddress = await this.contactsService.isAddressInContacts(fromAddress)
-            if (!hasContactBookAddress && !this.addressesNotOnContactBook.includes(fromAddress))
-              this.addressesNotOnContactBook.push(fromAddress)
-            else {
-              const name = await this.contactsService.getContactName(fromAddress)
-              if (name) this.airGapTxs[i].extra.names[fromAddress] = name
-            }
-          }
+  private async storeAddress(transaction: IAirGapTransaction) {
+    for (const address of [...transaction.from, ...transaction.to]) {
+      const hasContactBookAddress = await this.contactsService.isAddressInContacts(address)
+      if (!hasContactBookAddress) {
+        this.addressesNotOnContactBook.push(address)
+      } else {
+        const name = await this.contactsService.getContactName(address)
+        if (name) {
+          transaction.extra.names[address] = name
         }
       }
     }
+  }
+
+  public async checkAdressesNames() {
+    if (this.airGapTxs?.length <= 0) {
+      return
+    }
+
+    if (!(await this.contactsService.isBookEnabled())) {
+      return
+    }
+
+    this.addressesNotOnContactBook = []
+    this.airGapTxs.map((tx) => ({ ...tx, extra: { names: {} } })).forEach((tx) => this.storeAddress(tx))
   }
 
   async onClickDontAddContact(address: string) {
@@ -74,5 +64,4 @@ export class AddAddressComponent  implements OnInit {
     await this.contactsService.setBookEnable(false)
     this.addressesNotOnContactBook = []
   }
-
 }
