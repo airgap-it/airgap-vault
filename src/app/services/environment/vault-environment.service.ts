7686714595d8cb9d417d1f18c14e0721271833fd
Julia Samol <j.samol@papers.ch>
2023-08-31 11:56:06 +0200

    chore(): remove unused parameter

--- src/app/services/environment/vault-environment.service.ts
+++ src/app/services/environment/vault-environment.service.ts
@@ -1,12 +1,11 @@
 import { BaseEnvironmentService, RuntimeMode } from '@airgap/angular-core'
 import { Injectable } from '@angular/core'
-import { Platform } from '@ionic/angular'
 
 @Injectable({
   providedIn: 'root'
 })
 export class VaultEnvironmentService extends BaseEnvironmentService {
-  constructor(private readonly platform: Platform) {
+  constructor() {
     super(RuntimeMode.OFFLINE)
   }
 }
