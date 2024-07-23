2e8deebe328523da2c77357c14206c07659146fe
IsaccoSordo <isacco.sordo@gmail.com>
2024-07-23 11:16:40 +0200

    chore: init ios migration

---
+++ ios/App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Isacco on 23.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
