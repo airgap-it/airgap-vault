75b19d91240308c99d55f5c66fd3a07863045211
Recolic <git@me.recolic.net>
2026-07-25 01:01:13 -0700

    AI impl erc20 parse. squashed

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -2,6 +2,7 @@ import { IAirGapTransaction, ProtocolSymbols } from '@airgap/coinlib-core'
 import { Component, Input, OnInit } from '@angular/core'
 import { Observable } from 'rxjs'
 import { ContactsService } from 'src/app/services/contacts/contacts.service'
+import { parseERC20Data } from 'src/app/utils/erc20-data-parser'
 
 import { AggregatedDetails, TransactionStore } from './transaction.store'
 
@@ -21,6 +22,7 @@ export class TransactionComponent implements OnInit {
   public protocolIdentifier$: Observable<ProtocolSymbols | undefined>
   public airGapTxs$: Observable<IAirGapTransaction[]>
   public aggregatedDetails$: Observable<AggregatedDetails | undefined>
+  public readonly rawData: Map<IAirGapTransaction, [string, string]> = new Map()
 
   constructor(private readonly store: TransactionStore, private readonly contactsService: ContactsService) {
     this.protocolIdentifier$ = this.store.selectProtocolIdentifier()
@@ -38,6 +40,35 @@ export class TransactionComponent implements OnInit {
   public async ngOnChanges() {
     await this.setAddressNames()
     this.store.setAirGapTxs(this.airGapTxs)
+  }
+
+  public toggleData(tx: IAirGapTransaction): void {
+    const raw: [string, string] | undefined = this.rawData.get(tx)
+    if (raw !== undefined) {
+      const restoredTx: IAirGapTransaction = { ...tx, data: raw[0], to: [raw[1], ...tx.to.slice(1)] }
+      this.rawData.delete(tx)
+      this.replaceTransaction(tx, restoredTx)
+
+      return
+    }
+
+    const parsed: [string, string] | undefined = parseERC20Data(tx.data, tx.to[0])
+    if (parsed === undefined) {
+      return
+    }
+
+    const parsedTx: IAirGapTransaction = { ...tx, data: parsed[0], to: [parsed[1], ...tx.to.slice(1)] }
+    this.rawData.set(parsedTx, [tx.data, tx.to[0]])
+    this.replaceTransaction(tx, parsedTx)
+  }
+
+  private replaceTransaction(current: IAirGapTransaction, replacement: IAirGapTransaction): void {
+    if (this.airGapTxs === undefined) {
+      return
+    }
+
+    this.airGapTxs[this.airGapTxs.indexOf(current)] = replacement
+    this.store.setAirGapTxs([...this.airGapTxs])
   }
 
   private async setAddressNames() {
