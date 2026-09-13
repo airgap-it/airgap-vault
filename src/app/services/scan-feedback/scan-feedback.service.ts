d1d1ce726787dab3b0d5855bd27af9759ff6e5f4
Monica <318406572+5w4kjd95yk-eng@users.noreply.github.com>
2026-09-12 17:06:49 -0700

    test: make QR scan haptics mockable

--- src/app/services/scan-feedback/scan-feedback.service.ts
+++ src/app/services/scan-feedback/scan-feedback.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@angular/core'
-import { Haptics, ImpactStyle } from '@capacitor/haptics'
+import { Inject, Injectable } from '@angular/core'
+import { HapticsPlugin, ImpactStyle } from '@capacitor/haptics'
 import { Platform } from '@ionic/angular'
+
+import { HAPTICS_PLUGIN } from 'src/app/capacitor-plugins/injection-tokens'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScanFeedbackService {
-  constructor(private readonly platform: Platform) {}
+  constructor(private readonly platform: Platform, @Inject(HAPTICS_PLUGIN) private readonly haptics: HapticsPlugin) {}
 
   public notifyFrameAccepted(): void {
     if (!this.platform.is('android')) {
       return
     }
 
-    void Haptics.impact({ style: ImpactStyle.Light }).catch(() => undefined)
+    void this.haptics.impact({ style: ImpactStyle.Light }).catch(() => undefined)
   }
 }
